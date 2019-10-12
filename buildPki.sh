5dfd11867450c63775ca292728b3ddbabdef448a
rblundell <none>
2019-10-12 12:22:01 +0000

    add easy rsa installer

--- buildPki.sh
+++ buildPki.sh
@@ -7,6 +7,15 @@ INTERFACE="$(route -n| grep ^0.0.0.0|tr -s ' '|cut -d' ' -f8|grep '^[a-z].*[0-9]
 IP="$(ifconfig $INTERFACE | grep inet| tr -s ' '| cut -d' ' -f3|grep '^[0-9].*[0-9]$'|head -n1)"
 
 if [ ! -x "$EASYRSA" ]; then
+  wget -4 --no-check-certificate https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.6/EasyRSA-unix-v3.0.6.tgz
+  tar zxvf EasyRSA-unix-v3.0.6.tgz
+  rm -rf /usr/share/easy-rsa
+  mkdir -p /usr/share/easy-rsa
+  mv EasyRSA-v3.0.6 /usr/share/easy-rsa/3.0.6
+fi
+
+
+if [ ! -x "$EASYRSA" ]; then
 	echo "easy-rsa required ($EASYRSA)"
 	exit 1
 fi
