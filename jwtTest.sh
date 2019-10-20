a539e8c755c7b999021adb62dfb39329f06b2ccf
x <x>
2019-10-20 13:24:24 -0400

    add better jwt test case

--- jwtTest.sh
+++ jwtTest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 _VENV_PATH=~/.jwtVenv
+export ENCRYPTED_KEY_PASSPHRASE="12341234"
 export PRIV_KEY_FILE=pki/private/server.key
 export PUB_KEY_FILE=pki/issued/server.pub
 set -e
@@ -32,23 +33,27 @@ echo Spawned PID $sshAddProc_PID
 
 if [ -e test_key ]; then unlink test_key; fi
 if [ -e test_key.pub ]; then unlink test_key.pub; fi
+if [ -e test_key_encrypted ]; then unlink test_key_encrypted; fi
+if [ -e test_key_encrypted.pub ]; then unlink test_key_encrypted.pub; fi
 
 ssh-keygen -t rsa -N "" -f test_key
+ssh-keygen -t rsa -N "$ENCRYPTED_KEY_PASSPHRASE" -f test_key_encrypted
 ls -al test_key*
 
 VALID_TOKEN="$(PRIV_KEY_FILE=$PRIV_KEY_FILE PUB_KEY_FILE=$PUB_KEY_FILE ./_jwtTest.py --encode)"
-VALID_TOKEN="$(PRIV_KEY_FILE=pki/private/server.key PUB_KEY_FILE=pki/issued/server.pub ./_jwtTest.py --encode)"
-
-
 echo VALID_TOKEN=$VALID_TOKEN
 
 set +e
-
-SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN ssh-add -l
-SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN ssh-add test_key
+SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN ssh-add -l 2>/dev/null
+SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN ssh-add test_key 2>/dev/null
+SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN passh -p "$ENCRYPTED_KEY_PASSPHRASE" -P "Enter passphrase for test_key_encrypted:" ssh-add test_key_encrypted 2>/dev/null
 
 set -e
-SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN ssh-add -l | grep test_key
+SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN ssh-add -l 2>/dev/null | grep ' test_key (RSA'
+SSH_AUTH_SOCK=test_socket AUTH_TOKEN=$VALID_TOKEN ssh-add -l 2>/dev/null | grep ' test_key_encrypted (RSA'
+
+kill $sshAddProc_PID
 
 echo
-echo OK
+echo OK- Loaded test_key and test_key_encrypted
+
