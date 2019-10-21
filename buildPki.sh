0fa9a89a293fa1b0a6aa69f729a71b7556c9f7f7
x <x>
2019-10-21 12:35:04 -0400

    m

--- buildPki.sh
+++ buildPki.sh
@@ -3,8 +3,8 @@ set -e
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 origDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EASYRSA="/usr/share/easy-rsa/3.0.6/easyrsa"
-INTERFACE="$(route -n| grep ^0.0.0.0|tr -s ' '|cut -d' ' -f8|grep '^[a-z].*[0-9]$')"
-IP="$(ifconfig $INTERFACE | grep inet| tr -s ' '| cut -d' ' -f3|grep '^[0-9].*[0-9]$')"
+INTERFACE="$(route -n| grep ^0.0.0.0|tr -s ' '|cut -d' ' -f8|grep '^[a-z].*[0-9]$'|head -n1)"
+IP="$(ifconfig $INTERFACE | grep inet| tr -s ' '| cut -d' ' -f3|grep '^[0-9].*[0-9]$'|head -n1)"
 
 if [ ! -x "$EASYRSA" ]; then
 	echo "easy-rsa required ($EASYRSA)"
